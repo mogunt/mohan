6df452e2d6fca9f1f867f716b361a9f644d25557
mogunt <41136335+mogunt@users.noreply.github.com>
2018-07-12 14:52:24 +0530

    Update hunter2.py

--- hunter2.py
+++ hunter2.py
@@ -1,7 +1,13 @@
 n=int(input())
 s=input()
 s=s.split()
-s.sort()
-t="".join(s)
-t=t[::-1]
+sum=0
+for i in s:
+  sum=sum+int(i)
+if(sum==0):
+  print("0")
+else:
+  s.sort()
+  t="".join(s)
+  t=t[::-1]
 print(t)
