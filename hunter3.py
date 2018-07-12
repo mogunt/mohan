b5247fa86a754b530d79d7238de33bc64f4cecdd
mogunt <41136335+mogunt@users.noreply.github.com>
2018-07-12 15:13:57 +0530

    Update hunter3.py

--- hunter3.py
+++ hunter3.py
@@ -6,8 +6,11 @@ for i in range(0,n):
     if(i==int(s[i])):
         l.append(int(s[i]))
 l.sort()
-for i in range(0,len(l)):
-    if(i!=len(l)-1):
-        print(l[i],end=" ")
-    else:
-        print(l[i])
+if(len(l)==0):
+    print("-1")
+else:
+    for i in range(0,len(l)):
+        if(i!=len(l)-1):
+            print(l[i],end=" ")
+        else:
+            print(l[i])
