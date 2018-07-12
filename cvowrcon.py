6ab3fea8fcc7c3f1373d2447ffc07d7cafa3846a
mogunt <41136335+mogunt@users.noreply.github.com>
2018-07-12 12:50:02 +0530

    Update cvowrcon.py

--- cvowrcon.py
+++ cvowrcon.py
@@ -1,5 +1,7 @@
 c=input()
-if(c=='a' or c=='e' or c=='i' or c=='o' or 'u'):
+if(ord(c)<65):
+  print("Invalid")
+elif(c=='a' or c=='e' or c=='i' or c=='o' or 'u'):
   print("Vowel")
 else:
   print("Consonant")
