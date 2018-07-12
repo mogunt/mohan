ca89a61d467e0deacbe9a8e1fedab62d26cfccaf
mogunt <41136335+mogunt@users.noreply.github.com>
2018-07-12 14:29:18 +0530

    Create hunter1.py

---
+++ hunter1.py
@@ -0,0 +1,18 @@
+n=int(input())
+s=input()
+s=s.split()
+l=[]
+for i in range(0,len(s)):
+    s[i]=int(s[i])
+t=set(s)
+for i in t:
+    c=s.count(i)
+    if(c>1):
+        l.append(i)
+if(len(l)==0):
+        print("Unique")
+else:
+    l.sort()
+    for y in l:
+        print(y,end=""," ")
+               
